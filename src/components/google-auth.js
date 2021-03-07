37c428a3903ab5f9c5f496b75be7f5c84b4d975b
Aleksey Vaskin <alzor85@gmail.com>
2021-03-07 08:55:35 +0300

    Redux setup

--- src/components/google-auth.js
+++ src/components/google-auth.js
@@ -19,22 +19,6 @@ class GoogleAuth extends React.Component {
     });
   }
 
-  componentDidUpdate() {
-    window.gapi.load("client:auth2", () => {
-      window.gapi.client
-        .init({
-          clientId:
-            "932103381506-ft7fil3006o0vdr6p65u7o3t6tt5rsff.apps.googleusercontent.com",
-          scope: "email",
-        })
-        .then(() => {
-          this.auth = window.gapi.auth2.getAuthInstance();
-          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
-          this.auth.isSignedIn.listen(this.onAuthChange);
-        });
-    });
-  }
-
   onAuthChange = () => {
     this.setState({ isSignedIn: this.auth.isSignedIn.get() });
   };
